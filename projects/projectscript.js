22b62089124b6d7a36c3f48675fe6e3b04a7b21e
developmentHF <development.henry@gmail.com>
2023-02-06 00:42:41 +1100

    basic project showcase

---
+++ projects/projectscript.js
@@ -0,0 +1,11 @@
+const landingpage = document.querySelector("#landingpage");
+
+landingpage.addEventListener('click', () => {
+    window.location = "https://developmenthf.github.io/landing-page";
+})
+
+const pxljs = document.querySelector("#pxljs");
+
+landingpage.addEventListener('click', () => {
+    window.location = "https://developmenthf.github.io/pxl-js";
+})
