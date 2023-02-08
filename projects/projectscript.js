d698c3bc44d5fd4e70dc7ded2101138e45b22ad6
developmentHF <development.henry@gmail.com>
2023-02-08 21:08:48 +1100

    added project to project page

--- projects/projectscript.js
+++ projects/projectscript.js
@@ -2,10 +2,16 @@ const landingpage = document.querySelector("#landingpage");
 
 landingpage.addEventListener('click', () => {
     window.location = "https://developmenthf.github.io/landing-page";
-})
+});
 
 const pxljs = document.querySelector("#pxljs");
 
 pxljs.addEventListener('click', () => {
     window.location = "https://developmenthf.github.io/pxl-js";
-})
+});
+
+const PuckItUp = document.querySelector("#PuckItUp");
+
+PuckItUp.addEventListener('click', () => {
+    window.location = "https://youtube.com/shorts/SoRchPGsBxE?feature=share";
+});
